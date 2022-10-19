c2ac858ce75ac268f9045ee663dfd69afb186402
ses011 <egor54345@gmail.com>
2022-10-20 00:26:52 +0300

    add handler for get_balance api method

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -1,1 +1,22 @@
 package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (h *Handler) GetBalance(ctx *gin.Context) {
+	params := ctx.Request.URL.Query()
+
+	userId, ok := params["id"]
+	if !ok {
+		ctx.JSON(http.StatusBadRequest, "cant extract user id from query")
+	}
+
+	resp, err := h.ctrl.GetBalance(ctx, userId[0])
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+	}
+
+	ctx.JSON(http.StatusOK, resp)
+}
