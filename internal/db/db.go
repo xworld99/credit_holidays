13134efa4527f4dbd4882a9c01f8c8688e4ba949
ses011 <egor54345@gmail.com>
2022-10-20 00:29:20 +0300

    interface for db interaction

---
+++ internal/db/db.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"credit_holidays/internal/models"
+	"database/sql"
+)
+
+type CreditHolidaysDB interface {
+	// general
+	Begin(context.Context, sql.IsolationLevel) (*sql.Tx, error)
+	CommitTransaction(*sql.Tx) error
+	RollbackTransaction(*sql.Tx) error
+
+	// users interaction
+	GetUserById(context.Context, models.User) (models.User, error)
+	UpdateUser(context.Context, *sql.Tx, models.User) (models.User, error)
+	GetCreateUser(context.Context, *sql.Tx, models.User) (models.User, error)
+
+	// services interaction
+	GetServicesList(context.Context) ([]models.Service, error)
+	GetServiceById(context.Context, models.Service) (models.Service, error)
+
+	// orders interaction
+	CreateOrder(context.Context, *sql.Tx, models.Order) (models.Order, error)
+	UpdateOrder(context.Context, *sql.Tx, models.Order) (models.Order, error)
+	GetFullOrderInfo(
+		context.Context,
+		*sql.Tx,
+		models.Order,
+		models.User,
+		models.Service,
+	) (models.Order, models.User, models.Service, error)
+}
