a3c05147d2745301ee5dc3b45021d41eb3505a26
ses011 <egor54345@gmail.com>
2022-10-20 00:31:33 +0300

    services controller

---
+++ internal/controllers/service.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"context"
+	"credit_holidays/internal/models"
+)
+
+func (c *Controller) getServiceInfo(ctx context.Context, service models.Service) (models.Service, error) {
+	ctxTm, cancel := context.WithTimeout(ctx, c.selectTm)
+	defer cancel()
+
+	service, err := c.db.GetServiceById(ctxTm, service)
+	// check this error
+	if err != nil {
+		return models.Service{}, err
+	}
+
+	return service, nil
+}
