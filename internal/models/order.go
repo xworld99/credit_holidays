be100630d158db94245d0f59a682cc6413ad1216
ses011 <egor54345@gmail.com>
2022-10-20 00:28:47 +0300

    udpate models

--- internal/models/order.go
+++ internal/models/order.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type Order struct {
-	Id          int64
-	CreatedAt   time.Time
-	ProofedAt   time.Time
-	UserId      int64
-	ServiceId   uint
-	Amount      float64
-	Description string
-	Status      string
+	Id           int64     `json:"id"`
+	CreatedAt    time.Time `json:"create_at"`
+	ProofedAt    time.Time `json:"proofed_at,omitempty"`
+	ProofedAtStr string    `json:"-"`
+	UserId       int64     `json:"-"`
+	ServiceId    int64     `json:"-"`
+	Amount       float64   `json:"amount"`
+	Status       string    `json:"status"`
 }
