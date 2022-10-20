8ded94bd2bbfaaf0210c36b5059528b97470049b
ses011 <egor54345@gmail.com>
2022-10-21 01:17:53 +0300

    add logic for creating reports

---
+++ internal/models/report.go
@@ -0,0 +1,66 @@
+package models
+
+import (
+	"fmt"
+	"time"
+)
+
+type GetHistoryRequest struct {
+	UserId   string
+	FromDate string
+	ToDate   string
+	Offset   string
+	Limit    string
+	OrderBy  string
+}
+
+type History struct {
+	OrderId            int64     `json:"order_id"`
+	CreateAt           time.Time `json:"created_at"`
+	ProofedAt          time.Time `json:"proofed_at"`
+	Status             string    `json:"status"`
+	ServiceName        string    `json:"service_name"`
+	ServiceDescription string    `json:"service_description"`
+	ServiceType        string    `json:"service_type"`
+	Amount             float64   `json:"amount"`
+}
+
+type HistoryFrame struct {
+	TotalOperations int       `json:"total_operations"`
+	Offset          int       `json:"current_offset"`
+	Limit           int       `json:"-"`
+	FromDate        time.Time `json:"-"`
+	ToDate          time.Time `json:"-"`
+	OrderBy         string    `json:"-"`
+	UserId          int64     `json:"user_id"`
+	Operations      []History `json:"operations"`
+}
+
+type SaveReportRequest string
+
+type CSVRow struct {
+	Id       int64
+	Name     string
+	Type     string
+	CashFlow float64
+}
+
+type CSVData struct {
+	Period  time.Time
+	Records []CSVRow
+}
+
+func (c *CSVData) ToStringSlice() [][]string {
+	res := [][]string{{"ServiceId", "Name", "Type", "CashFlow"}}
+
+	for _, r := range c.Records {
+		res = append(res, []string{
+			fmt.Sprintf("%d", r.Id),
+			r.Name,
+			r.Type,
+			fmt.Sprintf("%.8f", r.CashFlow),
+		})
+	}
+
+	return res
+}
