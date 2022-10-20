02df49d5c0d54a1da619dd871ab0f8f19ecf6b6a
ses011 <egor54345@gmail.com>
2022-10-21 01:18:26 +0300

    add logic for creating reports

--- internal/controllers/report.go
+++ internal/controllers/report.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	"credit_holidays/internal/models"
+	"fmt"
 )
 
 func (c *Controller) GetServicesList(ctx context.Context) ([]models.Service, error) {
@@ -19,12 +20,10 @@ func (c *Controller) GetServicesList(ctx context.Context) ([]models.Service, err
 
 func (c *Controller) GetHistory(ctx context.Context, request models.GetHistoryRequest) (models.HistoryFrame, error) {
 	var err error
-	userId, fromDate, toDate, offset, limit, err := validateGetHistoryParams(request)
+	history, err := validateGetHistoryParams(request)
 	if err != nil {
 		return models.HistoryFrame{}, nil
 	}
-
-	history := models.HistoryFrame{UserId: userId, FromDate: fromDate, ToDate: toDate, Offset: offset, Limit: limit}
 
 	ctxTm, cancel := context.WithTimeout(ctx, c.selectTm)
 	defer cancel()
@@ -35,4 +34,42 @@ func (c *Controller) GetHistory(ctx context.Context, request models.GetHistoryRe
 	}
 
 	return history, nil
+}
+
+func (c *Controller) SaveReport(ctx context.Context, request models.SaveReportRequest) (string, error) {
+	var err error
+
+	csvdata, err := validateSaveReportParams(request)
+	if err != nil {
+		return "", err
+	}
+
+	ctxTm1, cancel1 := context.WithTimeout(ctx, c.selectTm)
+	defer cancel1()
+	order, err := c.db.GetLastOrderMonth(ctxTm1, csvdata.Period)
+	if err != nil {
+		return "", err
+	}
+
+	filepath := createReportPath(csvdata.Period, order.CreatedAt)
+	if fileAlreadyExists(c.staticPath, filepath) {
+		return filepath, nil
+	}
+
+	ctxTm, cancel := context.WithTimeout(ctx, c.selectTm)
+	defer cancel()
+
+	csvdata, err = c.db.FormReport(ctxTm, csvdata)
+	if err != nil {
+		return "", err
+	}
+
+	err = saveReport(c.staticPath, filepath, csvdata)
+	if err != nil {
+		return "", fmt.Errorf("internal server error")
+	}
+
+	deleteUnnecessaryReport(c.staticPath, filepath, csvdata.Period)
+
+	return filepath, nil
 }
