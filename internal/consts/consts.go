3d5eae7798056c97ef6f826fa54ae470be12a7ee
ses011 <egor54345@gmail.com>
2022-10-22 01:23:23 +0300

    update consts

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -1,4 +1,26 @@
 package consts
 
-var OrderActions = map[string]bool{"proof": true, "decline": true}
-var SortingType = map[string]bool{"created_at": true, "amount": true}
+import "net/http"
+
+const (
+	ProofedNow  = "now()"
+	ProofedNull = "null"
+
+	OrderSuccess    = "success"
+	OrderDeclined   = "declined"
+	OrderInProgress = "in_progress"
+	OrderProof      = "proof"
+	OrderDecline    = "decline"
+
+	OperationAccrual  = "accrual"
+	OperationWithdraw = "withdraw"
+)
+
+var (
+	OrderActions      = map[string]bool{OrderProof: true, OrderDecline: true}
+	SortingType       = map[string]bool{"created_at": true, "amount": true}
+	ErrorDescriptions = map[int]string{
+		http.StatusBadRequest:          "error in user request",
+		http.StatusInternalServerError: "cant handle request, internal error",
+	}
+)
