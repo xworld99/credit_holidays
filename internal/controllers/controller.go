7bb84de2dd9549f1e69e3fbc2d224c239ee6b403
ses011 <egor54345@gmail.com>
2022-10-22 01:24:04 +0300

    remove redutant consts + add close function

--- internal/controllers/controller.go
+++ internal/controllers/controller.go
@@ -8,23 +8,21 @@ import (
 
 // Controller provides all business logic for handler struct
 type Controller struct {
-	db            db.CreditHolidaysDB
-	selectTm      time.Duration
-	updateTm      time.Duration
-	deleteTm      time.Duration
-	insertTm      time.Duration
-	transactionTm time.Duration
-	staticPath    string
+	db         db.CreditHolidaysDB
+	dbTm       time.Duration
+	staticPath string
 }
 
 func NewController(cfg *koanf.Koanf, db db.CreditHolidaysDB) (*Controller, error) {
 	return &Controller{
-		db:            db,
-		selectTm:      cfg.Duration("timeout.db_select"),
-		updateTm:      cfg.Duration("timeout.db_update"),
-		deleteTm:      cfg.Duration("timeout.db_delete"),
-		insertTm:      cfg.Duration("timeout.db_insert"),
-		transactionTm: cfg.Duration("timeout.db_transaction"),
-		staticPath:    cfg.String("path.static"),
+		db:         db,
+		dbTm:       cfg.Duration("timeout.db_transaction"),
+		staticPath: cfg.String("path.static"),
 	}, nil
+}
+
+func (c *Controller) Close() {
+	if c.db != nil {
+		c.db.Close()
+	}
 }
