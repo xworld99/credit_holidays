b3ce9638dce7f81c1315784642e4be0bf693541b
ses011 <egor54345@gmail.com>
2022-10-22 01:28:08 +0300

    update postgres init script

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS users (
     -- unique id, not serial because of task text
     id int PRIMARY KEY,
     -- current active balance of user
-    balance money NOT NULL default 0.0 CHECK(balance >= 0.0),
+    balance money NOT NULL default 0::money CHECK(balance >= 0::money),
     -- reserved balance of user
-    frozen_balance money DEFAULT 0.0 CHECK(frozen_balance >= 0.0)
+    frozen_balance money DEFAULT 0::money CHECK(frozen_balance >= 0::money)
 );
 
 CREATE TABLE IF NOT EXISTS services (
@@ -15,18 +15,18 @@ CREATE TABLE IF NOT EXISTS services (
     -- should this service be proofed before changing balance of user
     confirmation_needed boolean default false,
     -- text description
-    desctiption text NOT NULL,
+    description text NOT NULL,
     -- specify if balance should be increased or decreased
     service_type text NOT NULL CHECK(service_type in ('accrual', 'withdraw')) DEFAULT 'withdraw'
 );
 
 INSERT INTO services(name, description, confirmation_needed, service_type)
-VALUES (('deposit', 'replenishment of the balance from an external source', false, 'accrual'),
-        ('withdraw', 'withdrawal of money to an external source', true, 'withdraw'),
-        ('transfer', 'transfer money from one user to another one', true, 'withdraw'),
-        ('subscription', 'withdrawal of money to an external source', true, 'withdraw'),
-        ('payment', 'user buy something using his virtual balance', true, 'withdraw')
-        ('cashback', 'cashback on user balance', false, 'accrual'));
+VALUES ('deposit', 'replenishment of the balance from an external source', false, 'accrual'),
+       ('withdraw', 'withdrawal of money to an external source', true, 'withdraw'),
+       ('transfer', 'transfer money from one user to another one', true, 'withdraw'),
+       ('subscription', 'withdrawal of money to an external source', true, 'withdraw'),
+       ('payment', 'user buy something using his virtual balance', true, 'withdraw'),
+       ('cashback', 'cashback on user balance', false, 'accrual');
 
 CREATE TABLE IF NOT EXISTS orders (
     -- unique id
@@ -37,6 +37,8 @@ CREATE TABLE IF NOT EXISTS orders (
     proofed_at timestamp DEFAULT null,
     user_id integer REFERENCES users(id) ON DELETE CASCADE,
     service_id INTEGER REFERENCES services(id) ON DELETE CASCADE,
-    amount money NOT NULL CHECK(amount >= 0.0),
+    amount money NOT NULL CHECK(amount >= 0::money),
     status text DEFAULT 'in_progress' CHECK(status in ('in_progress', 'success', 'declined'))
 );
+
+CREATE INDEX IF NOT EXISTS orders_created_at_idx ON orders USING btree(created_at);
