8d3e080e0c1f8bdb2cec5b4ffdfc0cedc824461c
ses011 <egor54345@gmail.com>
2022-10-22 01:22:25 +0300

    add new endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -33,20 +33,28 @@ func main() {
 	}
 
 	handler, err := handlers.InitializeHandler(cfg)
+	defer handler.Close()
+
 	if err != nil {
 		log.WithError(err).WithField("path", confPath).Fatal("can't init handler")
 	}
 
 	r := gin.Default()
+	r.Static("/reports", cfg.String("path.static"))
 
 	userGroup := r.Group("/user")
 	userGroup.GET("/get_balance", handler.GetBalance)
+	userGroup.GET("/get_history", handler.GetUserHistory)
 
 	orderGroup := r.Group("/order")
 	orderGroup.POST("/add_order", handler.AddOrder)
 	orderGroup.POST("/change_order_status", handler.ChangeOrderStatus)
 
-	r.Static("/reports", cfg.String("path.static"))
+	reportGroup := r.Group("/report")
+	reportGroup.GET("/save_report", handler.GenerateReport)
+
+	serviceGroup := r.Group("/service")
+	serviceGroup.GET("/get_all", handler.GetServicesList)
 
 	log.Fatal(r.Run(":8080"))
 }
