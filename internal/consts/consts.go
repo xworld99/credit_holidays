7120c4c8b39ca7bb989781180434e9439ce93414
ses011 <egor54345@gmail.com>
2022-10-22 19:46:26 +0300

    add new status for response

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -22,5 +22,6 @@ var (
 	ErrorDescriptions = map[int]string{
 		http.StatusBadRequest:          "error in user request",
 		http.StatusInternalServerError: "cant handle request, internal error",
+		http.StatusNotFound:            "not found",
 	}
 )
